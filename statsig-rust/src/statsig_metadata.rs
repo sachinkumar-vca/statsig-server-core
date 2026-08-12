15ff8f8b02f2f45a5c302586a564e4854de06fb1
Jacob O'Quinn <jacob.oquinn@amplitude.com>
2026-08-12 14:59:23 -0700

    [release] 0.21.0 - TLS-Inspection / Corporate Proxy Support (All SDKs), Entity List APIs (.NET/PHP/Elixir/C++), Sticky-Value Enforce Options (Node/Python/Go/.NET/PHP), Faster Java User Construction [rc] (#3125)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-rust/src/statsig_metadata.rs
+++ statsig-rust/src/statsig_metadata.rs
@@ -10,7 +10,7 @@ lazy_static! {
     static ref STATSIG_METADATA: RwLock<StatsigMetadata> = RwLock::new(StatsigMetadata::new());
 }
 
-pub const SDK_VERSION: &str = "0.20.3-rc.2608051827";
+pub const SDK_VERSION: &str = "0.21.0";
 
 const TAG: &str = stringify!(StatsigMetadata);
 
